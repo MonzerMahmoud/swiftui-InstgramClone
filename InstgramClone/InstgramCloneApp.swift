24bdcb8b5b621784ed1fc022f43be94e13133133
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2021-12-25 16:30:57 +0200

    Initial Commit

---
+++ InstgramClone/InstgramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstgramCloneApp.swift
+//  InstgramClone
+//
+//  Created by Syber Expertise on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstgramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
